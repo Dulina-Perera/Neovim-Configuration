7df7eabf41759fe9f7cb92fc021bc5973666e56c
Pyrokryos <dulina.perera@outlook.com>
2024-06-29 12:35:52 +0530

    Added few more options.

--- lua/dulina/core/options.lua
+++ lua/dulina/core/options.lua
@@ -1,35 +1,42 @@
-vim.cmd('let g:netrw_liststyle = 3')
-
+-- 📂lua/🌑settings.lua
+local global = vim.g
 local opt = vim.opt
 
-opt.number = true
-opt.relativenumber=true
+vim.scriptencoding = "utf-8"
 
--- Tabs & Indentation
-opt.tabstop = 2
-opt.expandtab = true
-opt.shiftwidth = 2
+global.netrw_liststyle = 3
+
+-- Map <leader>
+global.mapleader = " "
+global.maplocalleader = " "
+
+-- Editor options
 opt.autoindent = true
-
-opt.wrap = false
-
--- Search Settings
-opt.ignorecase = true
-opt.smartcase = true
-
+opt.background = "light"
+opt.backspace = "eol,indent,start"
+opt.clipboard = "unnamedplus"
 opt.cursorline = true
-
-opt.termguicolors = true
-opt.background = 'light'
-opt.signcolumn = 'yes'
-
--- Backspace
-opt.backspace = 'indent,eol,start'
-
--- Clipboard
-opt.clipboard:append('unnamedplus')
-
--- Split Windows
+opt.encoding = "utf-8"
+opt.expandtab = true
+opt.fileencoding = "utf-8"
+opt.hidden = true
+opt.ignorecase = true
+opt.inccommand = "split"
+opt.mouse = "a"
+opt.number = true
+opt.relativenumber = true
+opt.ruler = true
+opt.shiftwidth = 2
+opt.showcmd = true
+opt.showmatch = true
+opt.signcolumn = "yes"
+opt.smartcase = true
+opt.smartindent = true
 opt.splitright = true
-opt.splitbelow = true
-
+opt.syntax = "on"
+opt.tabstop = 2
+opt.termguicolors = true
+opt.title = true
+opt.ttimeoutlen = 0
+opt.wildmenu = true
+opt.wrap = true
