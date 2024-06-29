6c59734b2dba826c0a82b0d5d04b6e314216b4f6
Pyrokryos <dulina.perera@outlook.com>
2024-06-29 13:36:07 +0530

    Setup `which-key`.

--- lua/dulina/plugins/nvim-tree.lua
+++ lua/dulina/plugins/nvim-tree.lua
@@ -229,9 +229,9 @@ return {
 
     local keymap = vim.keymap
 
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { noremap = true, silent = true })
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { noremap = true, silent = true })
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { noremap = true, silent = true })
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { noremap = true, silent = true })
+    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
+    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
   end
 }
