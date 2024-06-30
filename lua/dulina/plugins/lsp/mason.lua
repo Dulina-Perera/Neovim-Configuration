5a9af5b9ced7bf74db7650e147c425370edc676d
Pyrokryos <dulina.perera@outlook.com>
2024-06-30 20:21:02 +0530

    Setup formatting.

--- lua/dulina/plugins/lsp/mason.lua
+++ lua/dulina/plugins/lsp/mason.lua
@@ -1,65 +1,76 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "angularls",
-        "asm_lsp",
-        "bashls",
-        "clangd",
-        "csharp_ls",
-        "omnisharp",
-        "cmake",
-        "cssls",
-        "dockerls",
-        "dotls",
-        "glslls",
-        "gradle_ls",
-        "html",
-        "jsonls",
-        "jdtls",
-        "tsserver",
-        "julials",
-        "kotlin_language_server",
-        "texlab",
-        "lua_ls",
-        "autotools_ls",
-        "marksman",
-        "mesonlsp",
-        "spectral",
-        "pyright",
-        "r_language_server",
-        "sorbet",
-        "rust_analyzer",
-        "sqlls",
-        "solidity",
-        "taplo",
-        "tailwindcss",
-        "volar",
-        "hydra_lsp"
-      },
-    })
-  end,
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"angularls",
+				"asm_lsp",
+				"bashls",
+				"clangd",
+				"csharp_ls",
+				"omnisharp",
+				"cmake",
+				"cssls",
+				"dockerls",
+				"dotls",
+				"glslls",
+				"gradle_ls",
+				"html",
+				"jsonls",
+				"jdtls",
+				"tsserver",
+				"julials",
+				"kotlin_language_server",
+				"texlab",
+				"lua_ls",
+				"autotools_ls",
+				"marksman",
+				"mesonlsp",
+				"spectral",
+				"pyright",
+				"r_language_server",
+				"sorbet",
+				"rust_analyzer",
+				"sqlls",
+				"solidity",
+				"taplo",
+				"tailwindcss",
+				"volar",
+				"hydra_lsp",
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+			},
+		})
+	end,
 }
-
